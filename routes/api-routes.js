381b4d02a775c67da2981e515f80e04bdbca5bdc
Celine Ross <celineross@gmail.com>
2020-11-18 22:38:08 -0600

    added env file, finished api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,8 +4,37 @@ const mongoose = require("mongoose");
 //displays all workouts
 module.exports = function (app) {
     app.get("/api/workouts", (req, res) => {
-        db.Workout.find({}).then(workouts => {
-            res.json(workouts);
+        db.Workout.find({}).then(workout => {
+            res.json(workout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+    //creates new workout
+    app.post("/api/workouts", ({ body }, res) => {
+        db.Workout.create(body).then(workout => {
+            res.json(workout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+    //update exisiting workout
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+            req.params.id, { $push: { exercises: req.body } },
+            { new: true, runValidators: true }).then(workout => {
+                res.json(workout);
+            }).catch(err => {
+                res.json(err);
+            });
+    });
+
+    //find a workout in a certain range
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({}).then(workout => {
+            res.json(workout);
         }).catch(err => {
             res.json(err);
         });
