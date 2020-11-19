eebce572f1e9dc51f55187e7119463656291fd16
Celine Ross <celineross@gmail.com>
2020-11-18 21:12:51 -0600

    bones of README added homework folder code fixed up packages built model and and server need to delete develop folder

---
+++ Develop/models/Workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//define new schema and properties
+const workoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+});
+
+//assign model to a variable and export it
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Fitness-Tracker
+
+## Deployed
+* [Deployed App]()
+* [Repository]()
+
+## Author
+<p>Celine Ross</p>
+
+## Description
+<p></p>
+
+## Requirements
+<p></p>
+
+## Installation
+<p></p>
+
+## Usage
+<p></p>
+
+## License
+<p></p>
+
+## Contribution
+<p></p>
